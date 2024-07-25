98ed172d81725581ba0ea05c62cad64de6c8ae02
Anurag kumar <114020159+anuragkrgupta@users.noreply.github.com>
2024-07-25 14:59:02 +0530

    script.js

--- script.js
+++ script.js
@@ -13,12 +13,17 @@ const addTodoListToLS = (todo) => {
 }
 
 // Function to add todo item to list
-const addTodoItem = (inputValue) => {
+const addTodoItem = (inputValue, done = false) => {
   let listItem = document.createElement("li");
   listItem.innerHTML = `${inputValue}<i></i>`;
 
+  if (done) {
+    listItem.classList.add('done');
+  }
+
   listItem.addEventListener("click", function(){
     this.classList.toggle('done');
+    updateTodoItemInLS(inputValue, this.classList.contains('done'));
   })
 
   listItem.querySelector('i').addEventListener("click", function(){
@@ -29,16 +34,28 @@ const addTodoItem = (inputValue) => {
   list.appendChild(listItem);
 }
 
+// Function to update todo item in local storage
+const updateTodoItemInLS = (inputValue, done) => {
+  todoListValue = getTodoListFromLS();
+  todoListValue = todoListValue.map(item => {
+    if (item.value === inputValue) {
+      item.done = done;
+    }
+    return item;
+  });
+  addTodoListToLS(todoListValue);
+}
+
 // Function to remove todo item from local storage
 const removeTodoItemFromLS = (inputValue) => {
   todoListValue = getTodoListFromLS();
-  todoListValue = todoListValue.filter(item => item !== inputValue);
+  todoListValue = todoListValue.filter(item => item.value !== inputValue);
   addTodoListToLS(todoListValue);
 }
 
 // Load existing todo items from local storage
 todoListValue = getTodoListFromLS();
-todoListValue.forEach(item => addTodoItem(item));
+todoListValue.forEach(item => addTodoItem(item.value, item.done));
 
 // Event listener for input box
 inputbx.addEventListener("keyup", function(event){
@@ -47,7 +64,7 @@ inputbx.addEventListener("keyup", function(event){
 
     if(inputValue !== "") {
       todoListValue = getTodoListFromLS();
-      todoListValue.push(inputValue);
+      todoListValue.push({ value: inputValue, done: false });
       addTodoListToLS(todoListValue);
       addTodoItem(inputValue);
       this.value = "";
